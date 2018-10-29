e29383d1feeed4528cf518f5b3cc5e99d1dc0bf3
Pravin Arote <pravin.arote@trafigura.com>
2018-10-29 13:57:26 +0530

    Initial UI structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './ui/add-task/add-task.component';
+import { ViewTaskComponent } from './ui/view-task/view-task.component';
+import { EditTaskComponent } from './ui/edit-task/edit-task.component';
+import { AddProjectComponent } from './ui/add-project/add-project.component';
+import { AddUserComponent } from './ui/add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    ViewTaskComponent,
+    EditTaskComponent,
+    AddProjectComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule
