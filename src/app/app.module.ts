44246bca040e91f2e7bc91298fe75b735fd05cf8
Pravin Arote <pravin.arote@trafigura.com>
2018-10-29 16:23:25 +0530

    Added routing changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router'
+import { FormsModule,ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { AddTaskComponent } from './ui/add-task/add-task.component';
 import { ViewTaskComponent } from './ui/view-task/view-task.component';
 import { EditTaskComponent } from './ui/edit-task/edit-task.component';
 import { AddProjectComponent } from './ui/add-project/add-project.component';
 import { AddUserComponent } from './ui/add-user/add-user.component';
+
+
+const appRoutes:Routes=[
+  { path:'AddProject',component:AddProjectComponent },
+  { path:'AddTask', component:AddTaskComponent},
+  { path:'AddUser', component:AddUserComponent},
+  { path:'ViewTask', component:ViewTaskComponent},
+  { path:'EditTask/:id', component:EditTaskComponent},
+]
 
 @NgModule({
   declarations: [
@@ -18,7 +31,8 @@ import { AddUserComponent } from './ui/add-user/add-user.component';
     AddUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule, RouterModule.forRoot(appRoutes),
+    HttpClientModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
