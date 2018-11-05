fb056788899c928c84ecad45f757b6bec67b979d
Pravin Arote <pravin.arote@trafigura.com>
2018-11-05 18:04:05 +0530

    Updated the task

--- src/app/ui/add-task/add-task.component.ts
+++ src/app/ui/add-task/add-task.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DialogService } from 'ng2-bootstrap-modal';
 import { SearchComponent } from '../search/search.component';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { ProjectManagerServiceService } from 'src/app/services/project-manager-service.service';
+import { User } from 'src/app/models/user';
+import { Task } from 'src/app/models/task';
 
 @Component({
   selector: 'app-add-task',
@@ -9,9 +13,39 @@ import { SearchComponent } from '../search/search.component';
 })
 export class AddTaskComponent implements OnInit {
 
-  constructor(private dialogService:DialogService) { }
+  operation : string;
+  angularForm: FormGroup;
+  error : any = { isError : false, errorMessage : ''};
+  task : Task;
+
+  constructor(private service : ProjectManagerServiceService, private fb: FormBuilder, private dialogService:DialogService) { 
+    this.createForm();
+    this.task = new Task();
+  }
 
   ngOnInit() {
+  }
+
+  compareTwoDates() {
+    if(new Date(this.angularForm.controls['endDate'].value)<new Date(this.angularForm.controls['startDate'].value)){
+        this.error={isError:true,errorMessage:'End Date should not greater than start date.'};
+    }
+    else {
+      this.error={isError:false};
+    }
+  }
+
+  createForm() {
+    this.operation = "Add";
+    this.angularForm = this.fb.group({
+      projectName: new FormControl({value:'',disabled : true}, Validators.required),
+      taskName : new FormControl({},Validators.required),
+      priority : new FormControl(),
+      parentTask : new FormControl(),
+      startDate : new FormControl({value : new Date(), disabled : true}),
+      endDate : new FormControl({value : new Date(),disabled : true}),
+      userName : new FormControl({value:'',disabled : true}),
+    });
   }
 
 
