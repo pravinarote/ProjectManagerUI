5a2da0a1d60ac5eee59625c8a3e70e0931a2c64b
Pravin Arote <pravin.arote@trafigura.com>
2018-11-06 09:46:32 +0530

    Added validations

--- src/app/ui/add-task/add-task.component.ts
+++ src/app/ui/add-task/add-task.component.ts
@@ -16,11 +16,23 @@ export class AddTaskComponent implements OnInit {
   operation : string;
   angularForm: FormGroup;
   error : any = { isError : false, errorMessage : ''};
-  task : Task;
+  task : Task ={
+    EndDate : null,
+    IsTaskEnded : null,
+    ParentTaskId : null,
+    ParentTaskName : null,
+    Priority : 0,
+    ProjectId : null,
+    ProjectName : null,
+    StartDate : null,
+    TaskId : null,
+    TaskName : null,
+    UserId : null,
+    UserName : null
+  };
 
   constructor(private service : ProjectManagerServiceService, private fb: FormBuilder, private dialogService:DialogService) { 
     this.createForm();
-    this.task = new Task();
   }
 
   ngOnInit() {
@@ -41,11 +53,32 @@ export class AddTaskComponent implements OnInit {
       projectName: new FormControl({value:'',disabled : true}, Validators.required),
       taskName : new FormControl({},Validators.required),
       priority : new FormControl(),
-      parentTask : new FormControl(),
-      startDate : new FormControl({value : new Date(), disabled : true}),
-      endDate : new FormControl({value : new Date(),disabled : true}),
+      parentTaskCheckbox : new FormControl(),
+      parentTask : new FormControl({value:'',disabled : true}),
+      startDate : new FormControl({value : new Date(), disabled : false}),
+      endDate : new FormControl({value : new Date(),disabled : false}),
       userName : new FormControl({value:'',disabled : true}),
     });
+  }
+
+  parentTaskChecked(checked : boolean) {
+    if(checked) {
+      this.angularForm.controls['priority'].disable();
+      this.angularForm.controls['startDate'].disable();
+      this.angularForm.controls['endDate'].disable();
+      this.task.ProjectName = '';
+      this.task.Priority=0;
+      this.task.StartDate =null;
+      this.task.EndDate=null;
+      this.task.UserName=null;
+      this.task.UserId=null;
+      this.task.ProjectId=null;
+    }
+    else {
+      this.angularForm.controls['priority'].enable();
+      this.angularForm.controls['startDate'].enable();
+      this.angularForm.controls['endDate'].enable();
+    }
   }
 
 
